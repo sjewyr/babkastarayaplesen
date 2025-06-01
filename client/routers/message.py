1612a3eef3e976dca2fc792c79e1a3f7e077a6c6
sjewyr <ganshin.2004@gmail.com>
2025-06-01 18:33:17 +0300

    fix: fixed routers

--- client/routers/message.py
+++ client/routers/message.py
@@ -1,6 +1,7 @@
 import logging
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
+from client.usecases.dtos import IncomingMessage
 from dependencies.db_connection import get_db_connection
 from usecases.get_message import get_message_usecase
 from usecases.send_mesage import send_message_usecase
@@ -9,10 +10,10 @@ message_router = APIRouter(prefix="/message")
 
 
 @message_router.post("/get_message")
-def get_message(db=Depends(get_db_connection)):
-    return get_message_usecase()
+def get_message(request: Request, msg: IncomingMessage):
+    return get_message_usecase(request, msg)
 
 
 @message_router.post("/send_message")
-def send_message():
-    return send_message_usecase()
+def send_message(client_id: int, msg: str):
+    return send_message_usecase(client_id, msg)
